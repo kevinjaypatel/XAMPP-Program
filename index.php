266f0cc3789017c279e9b6efafb48ca4ac423e48
Kevin Patel <kevinjaypatel@gmail.com>
2020-04-01 11:37:50 -0700

    copied source html from csulb

--- index.php
+++ index.php
@@ -2,29 +2,37 @@
 <html>
 <head>
 	<title>Reset Password</title>
+    <link href="https://sites.csulb.edu/common/bootstrap-master/css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://sites.csulb.edu/common/beachid/assets/css/custom.css" rel="stylesheet">
+	<link href="../App_Themes/Default/styles.css" type="text/css" rel="stylesheet">
 </head>		
 <body>
 
 <header>
-	<nav>
-		<a href="https://www.csulb.edu/">
-			<img src="https://www.csulb.edu/sites/all/themes/custom/lb_theme/images/logo.png" alt="California State University, Long Beach">
-		</a>
-		<a href="https://www.csulb.edu/">CSULB</a>
-		<button type="button" data-toggle="collapse"></button>
-		<div>
-        <ul>
-          <li>
-            <a href="/">Home
-              <span>(current)</span>
+	<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+  
+      <a class="navbar-brand" href="https://www.csulb.edu" target="_self" role="heading"><img class="site-logo" src="https://www.csulb.edu/sites/all/themes/custom/lb_theme/images/logo.png" alt="California State University, Long Beach"></a>
+    <a class="mobile-brand" href="https://www.csulb.edu" target="_self" role="heading">CSULB</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="true" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="navbar-collapse collapse" id="navbarResponsive" style="">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active" id="homelink">
+            <a class="nav-link" href="/">Home
+              <span class="sr-only">(current)</span>
             </a>
           </li>
-          <li>
-            <a href="#" onclick="window.open('https://csulb.teamdynamix.com/TDClient/1993/Portal/Requests/ServiceDet?ID=28148','help','width=1024,height=700,scrollbars=1');return false;" target="_blank">Help</a>
+          <li class="nav-item" id="helplink">
+            <a class="nav-link" href="#" onclick="window.open('https://csulb.teamdynamix.com/TDClient/1993/Portal/Requests/ServiceDet?ID=28148','help','width=1024,height=700,scrollbars=1');return false;" target="_blank">Help</a>
           </li>
+          
         </ul>
-    	</div>
-    </nav>
+      
+    
+    </div>
+    
+  </nav>
 </header>
 
 </body>
