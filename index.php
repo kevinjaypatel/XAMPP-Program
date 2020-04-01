b52a0791cec21adeced78807e3566d48a73caea2
Kevin Patel <kevinjaypatel@gmail.com>
2020-04-01 11:28:22 -0700

    added styles folder and changed title for index file

--- index.php
+++ index.php
@@ -1,13 +1,31 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Title of the document</title>
+	<title>Reset Password</title>
 </head>		
 <body>
 
-<?php
-	echo "Hi there"; 
-?>
+<header>
+	<nav>
+		<a href="https://www.csulb.edu/">
+			<img src="https://www.csulb.edu/sites/all/themes/custom/lb_theme/images/logo.png" alt="California State University, Long Beach">
+		</a>
+		<a href="https://www.csulb.edu/">CSULB</a>
+		<button type="button" data-toggle="collapse"></button>
+		<div>
+        <ul>
+          <li>
+            <a href="/">Home
+              <span>(current)</span>
+            </a>
+          </li>
+          <li>
+            <a href="#" onclick="window.open('https://csulb.teamdynamix.com/TDClient/1993/Portal/Requests/ServiceDet?ID=28148','help','width=1024,height=700,scrollbars=1');return false;" target="_blank">Help</a>
+          </li>
+        </ul>
+    	</div>
+    </nav>
+</header>
 
 </body>
 </html>
